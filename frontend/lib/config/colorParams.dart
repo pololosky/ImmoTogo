97a8596cffdc502d7f1e16ed2317031b916ab62b
pololosky <follivios@gmail.com>
2025-11-25 19:53:19 +0000

    petite config

---
+++ frontend/lib/config/colorParams.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  static const Color primaryColor = Color.fromARGB(255, 59, 130, 246);
+  static const Color secondaryColor = Color.fromARGB(255, 30, 58, 138);
+  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color textColor = Color.fromARGB(255, 31, 41, 55);
+  static const Color unselectedItemColor = Color.fromARGB(255, 89, 81, 81);
+}
